701052dbf8a3bb1caf4bde80d2829fd7f169b527
johyewon <ixxwxfjlwf@gmail.com>
2021-11-24 12:06:01 +0900

    migrate to compose

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -29,6 +31,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.samples.apps.sunflower.R
@@ -65,6 +68,17 @@ class PlantDetailFragment : Fragment() {
                         plantDetailViewModel.addPlantToGarden()
                         Snackbar.make(root, R.string.added_plant_to_garden, Snackbar.LENGTH_LONG)
                             .show()
+                    }
+                }
+            }
+
+            composeView.apply {
+                setViewCompositionStrategy(
+                    ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+                )
+                setContent {
+                    MdcTheme {
+                        PlantDetailDescription(plantDetailViewModel)
                     }
                 }
             }
@@ -112,37 +126,38 @@ class PlantDetailFragment : Fragment() {
         return binding.root
     }
 
+
     // Helper function for calling a share functionality.
-    // Should be used when user presses a share button/menu item.
-    @Suppress("DEPRECATION")
-    private fun createShareIntent() {
-        val shareText = plantDetailViewModel.plant.value.let { plant ->
-            if (plant == null) {
-                ""
-            } else {
-                getString(R.string.share_text_plant, plant.name)
-            }
+// Should be used when user presses a share button/menu item.
+@Suppress("DEPRECATION")
+private fun createShareIntent() {
+    val shareText = plantDetailViewModel.plant.value.let { plant ->
+        if (plant == null) {
+            ""
+        } else {
+            getString(R.string.share_text_plant, plant.name)
         }
-        val shareIntent = ShareCompat.IntentBuilder.from(requireActivity())
-            .setText(shareText)
-            .setType("text/plain")
-            .createChooserIntent()
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-        startActivity(shareIntent)
     }
+    val shareIntent = ShareCompat.IntentBuilder.from(requireActivity())
+        .setText(shareText)
+        .setType("text/plain")
+        .createChooserIntent()
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+    startActivity(shareIntent)
+}
 
-    // FloatingActionButtons anchored to AppBarLayouts have their visibility controlled by the scroll position.
-    // We want to turn this behavior off to hide the FAB when it is clicked.
-    //
-    // This is adapted from Chris Banes' Stack Overflow answer: https://stackoverflow.com/a/41442923
-    private fun hideAppBarFab(fab: FloatingActionButton) {
-        val params = fab.layoutParams as CoordinatorLayout.LayoutParams
-        val behavior = params.behavior as FloatingActionButton.Behavior
-        behavior.isAutoHideEnabled = false
-        fab.hide()
-    }
+// FloatingActionButtons anchored to AppBarLayouts have their visibility controlled by the scroll position.
+// We want to turn this behavior off to hide the FAB when it is clicked.
+//
+// This is adapted from Chris Banes' Stack Overflow answer: https://stackoverflow.com/a/41442923
+private fun hideAppBarFab(fab: FloatingActionButton) {
+    val params = fab.layoutParams as CoordinatorLayout.LayoutParams
+    val behavior = params.behavior as FloatingActionButton.Behavior
+    behavior.isAutoHideEnabled = false
+    fab.hide()
+}
 
-    interface Callback {
-        fun add(plant: Plant?)
-    }
+interface Callback {
+    fun add(plant: Plant?)
+}
 }
